1c405d80aad775e27f7236a8cf40ff45afba014a
christoffsen <christopherelder@outlook.com>
2020-10-11 13:46:44 -0700

    Initial port of BandTest

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates&display=swap');
+*{
+	box-sizing: border-box;
+	font-family: Montserrat, Helvetica, Arial, sans-serif;
+}
+#app {
+	border-style: hidden;
+	border-radius: 0;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	background-color: #FCF6F5FF;
+	width: inherit;
+	min-width: 40rem;
+}
+#home {
+	border-style: hidden;
+	border-radius: 0;
+}
+
+button {
+	border-radius: 1rem;
+	background-color: gold;
+	box-shadow: none;
+	border: none;
+	cursor: pointer;
+	font-size: 2rem;
+	color: white;
+	margin: 1rem;
+}
+
+button:hover {
+	background-color: white;
+	color: gold;
+}
+
+h1 {
+	font-family: Pacifico, cursive;
+	font-size: 5rem;
+	color: #990011FF;
+}
+h2 {
+	color: #000000;
+	font-style: italic;
+	background-color: #FFFFFF;
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+	border-radius: 1rem;
+	width: 33%;
+}
+
+div {
+	border-width: 1px;
+	border-radius: 1rem;
+	border-color: #880000DD;
+	border-style: solid;
+	width: 100%;
+	overflow-x: auto;
+	padding-bottom: 5rem;
+}
+
+.song-choice-container {
+	padding: 1rem;
+}
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+	<div id="home">
+		<h1>Roadtrippers</h1>
+		<div class="song-choice-container">
+			<button v-for="song in songs" :key="song.name" @click="changeSong(song)">{{song.name}}</button>
+		</div>
+		<div v-if="currentSong !== null">
+			<h2>{{currentSong.name}}</h2>
+			<video controls width="800" height="600" :src="currentSong.link" type="video/mp4"></video>
+		</div>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+	name: 'Home',
+	data() {
+		return {
+			currentSong: null,
+			songs: [
+				{"name" : "Locomotive Breath", "link" : "https://band-videos.s3.ca-central-1.amazonaws.com/Locomotive_Breath.mp4"}
+				,{"name" : "Proud Mary", "link" : "https://band-videos.s3.ca-central-1.amazonaws.com/Proud_Mary.mp4"}
+				,{"name" : "I Saw Her Standing There", "link" : "https://band-videos.s3.ca-central-1.amazonaws.com/I_Saw_Her_Standing_There.mp4"}
+				,{"name" : "Last Kiss", "link" : "https://band-videos.s3.ca-central-1.amazonaws.com/Last_Kiss.mp4"}
+				,{"name" : "Keep on Runnin'", "link" : "https://band-videos.s3.ca-central-1.amazonaws.com/Keep_on_Runnin.mp4"}
+				,{"name" : "No Matter What", "link" : "https://band-videos.s3.ca-central-1.amazonaws.com/No_Matter_What.mp4"}
+				,{"name" : "Dead Flowers", "link" : "https://band-videos.s3.ca-central-1.amazonaws.com/Dead_Flowers.mp4"}
+				,{"name" : "I Hear You Knocking", "link" : "https://band-videos.s3.ca-central-1.amazonaws.com/I_Hear_You_Knocking.mp4"}
+			]
+		}
+	},
+	methods: {
+		changeSong(song) {
+			this.currentSong = song;
+
+		}
+	},
+	components: {
+	}
+}
+</script>
